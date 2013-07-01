ed518d4b8d7ccb2a2f7bb3296f5541ea0dc29375
Patricia Canaan <patricia.canaan@okstate.edu>
2013-07-01 15:33:31 -0400

    added some super importnat stuff

--- add_numb.py
+++ add_numb.py
@@ -1,8 +1,12 @@
 #This has been authored by Patricia, anyone can contribute
+from adding import add_two, add_three
+
 def add_two(a,b):
    return a+b
 def main():
+   print ("2 + 2 + 2 = ",add_three(2,2,2))
    print ("2 + 3 = ",add_two(2,3))
-   print ("2 + 4 = ",add_two(2,4))
+   pring ("Hello OK")
+
 if __name__ == "__main__":
      main()
