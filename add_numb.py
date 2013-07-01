99f57bdfd7f43ae8409c33923e0ecd6b635a5c85
Patricia Canaan <patricia.canaan@okstate.edu>
2013-07-01 14:48:28 -0400

    added dcoumentaion

---
+++ add_numb.py
@@ -0,0 +1,7 @@
+#This has been authored by Patricia, anyone can contribute
+def add_two(a,b):
+   return a+b
+def main():
+   print ("2 + 3 = ",add_two(2,3))
+if __name__ == "__main__":
+     main()
